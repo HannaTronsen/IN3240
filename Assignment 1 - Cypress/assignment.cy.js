10d94cea74b057aff9d7af8c974cac61afe211d9
Hanna Tronsen <hanna.tronsen@airthings.com>
2023-03-18 13:42:55 +0100

    changed cypress file

--- Assignment 1 - Cypress/assignment.cy.js
+++ Assignment 1 - Cypress/assignment.cy.js
@@ -13,18 +13,13 @@ describe('Cypress Assignment - Test Cases', () => {
   const zipCode = 'TestZipCode'
   //const phone = TestPhone --> This value isn't used to recover a lost account
   const ssn = 'TestSSN'
-  let userName = 'CypressUserName'
+  const userName = 'CypressUserName'
   const password = 'CypressPassword'
 
   beforeEach(() => {
     // Since we want to visit ParaBank URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
     cy.visit('https://parabank.parasoft.com/parabank/index.htm')
-
-    //The prerequisit for this test is to have an invalid username
-    if (Cypress.currentTest.title === 'Recover account succesfully') {
-      userName = 'WrongCypressUserName'
-    }
 
     //logging in
     cy.get('input[name=username]').type(`${userName}`)
@@ -51,24 +46,11 @@ describe('Cypress Assignment - Test Cases', () => {
     })
   })
 
-  it('Recover account succesfully', () => {
-    //After entering a wrong user name, we verify that we have the wrong credentials
-    cy.get('.error').should('have.text', 'The username and password could not be verified.')
-
-    cy.get('a:contains("Forgot login info?")').click()
-
-    cy.get('input[name=firstName]').type(`${firstName}`)
-    cy.get('input[name=lastName]').type(`${lastName}`)
-    cy.get('input[name=address\\.street]').type(`${street}`)
-    cy.get('input[name=address\\.city]').type(`${city}`)
-    cy.get('input[name=address\\.state]').type(`${state}`)
-    cy.get('input[name=address\\.zipCode]').type(`${zipCode}`)
-    cy.get('input[name=address\\.zipCode]').type(`${zipCode}`)
-    cy.get('input[name=ssn]').type(`${ssn}`)
-
-    cy.contains('input[type="submit"]', 'Find My Login Info').click()
-    //Verifies that after entering our correct lookup info, we are then succesfully logged in
-    cy.get('p:contains("Your login information was located successfully. You are now logged in")').should('exist')
+  it('Open new account funds', () => {
+    cy.get('a[href="/parabank/openaccount.htm"]').click()
+    cy.wait(900)
+    cy.get('input[type=submit]').click()
+    cy.get('.title').should('have.text', 'Account Opened!')
   })
 
   afterEach(() => {
